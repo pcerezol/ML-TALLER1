3d9acda527e3cdc67e4d17472f324a724e290086
pcerezol <107124469+pcerezol@users.noreply.github.com>
2022-06-20 16:06:24 -0500

    Create ProblemSET1_Cerezo_Durán.R

---
+++ Scripts/ProblemSET1_Cerezo_Durán.R
@@ -0,0 +1,6 @@
+######################################################
+
+#Problem Set 1: Predicting Income#
+
+# Authors: Pablo Cerezo Lesmes & Juan Sebasti??n Dur??n Dur??n
+######################################################
