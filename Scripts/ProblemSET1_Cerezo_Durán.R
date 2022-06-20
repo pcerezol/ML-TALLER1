4454005c807e0c14a773e22574aa059832fc679b
Juan <js.duran10@uniandes.edu.co>
2022-06-20 16:08:06 -0500

    Scraping
    Scraping

--- Scripts/ProblemSET1_Cerezo_Durán.R
+++ Scripts/ProblemSET1_Cerezo_Durán.R
@@ -4,3 +4,25 @@
 
 # Authors: Pablo Cerezo Lesmes & Juan Sebasti??n Dur??n Dur??n
 ######################################################
+
+# 1.a Scrape the data at the website #
+
+####################################################
+
+#cargamos el url--------------------------------------
+rm(list = ls())
+#Cargamos los paquetes necesarios----------------------
+library(tidyverse)
+library(rvest)
+library(pacman)
+library(dplyr)
+library(readr, warn.conflicts = FALSE)
+#Cargamos la data--------------------------------------
+GEIH <- data.frame()
+for (i in 1:10){
+  url <-paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_",i,".html")
+  temp <- read_html(url)%>%
+    html_table()  
+  temp <- as.data.frame(temp)
+  GEIH <- rbind(GEIH,temp)
+}
