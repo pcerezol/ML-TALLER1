c4cdbf015d36196f5f2dfbb750396dd45ebb4d59
pcerezol <107124469+pcerezol@users.noreply.github.com>
2022-06-21 21:05:14 -0500

    Update 04_pablo.R

--- Scripts/04_pablo.R
+++ Scripts/04_pablo.R
@@ -14,3 +14,35 @@ SE2 <- function(GEIH_clean, index){
   coef(lm(log_Ing~female, data=GEIH_clean), data=GEIH_clean, subset = index)
 }
 boot(data=GEIH_clean, SE2, R=1000)
+summary(GEIH_clean$female)
+###################
+#La base únicamente contiene información de Bogotá
+##############
+#Creamos sets de data solo con hombres y mujeres
+GEIH_mas<-subset(GEIH_clean, female==0)
+GEIH_fem<-subset(GEIH_clean, female==1)
+
+#regresión edad para hombres
+modelo2_mas <- lm(log_Ing~age, data=GEIH_mas)
+summary(modelo2_mas)
+SE2_mas <- function(GEIH_mas, index){
+  coef(lm(log_Ing~age, data=GEIH_mas), data=GEIH_mas, subset = index)
+}
+boot(data=GEIH_mas, SE2_mas, R=1000)
+
+#regresión edad para mujeres
+modelo2_fem <- lm(log_Ing~age, data=GEIH_fem)
+summary(modelo2_fem)
+
+SE2_fem <- function(GEIH_fem, index){
+  coef(lm(log_Ing~age, data=GEIH_fem), data=GEIH_fem, subset = index)
+}
+boot(data=GEIH_fem, SE2_fem, R=1000)
+
+
+
+
+##########4.5.4
+
+modelo3<-lm(log_Ing~female+age+relab+formal+educ+p6426, data=GEIH_clean)
+summary(modelo3)
