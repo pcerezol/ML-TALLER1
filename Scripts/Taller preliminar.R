de44b6e2db2a8bd1fe85a3886c5587cf012cb2e1
pcerezol <107124469+pcerezol@users.noreply.github.com>
2022-06-27 19:16:07 -0500

    upcate

--- Scripts/Taller preliminar.R
+++ Scripts/Taller preliminar.R
@@ -61,7 +61,7 @@ for (i in 1:10){
 #Dejamos mayores de edad y trabajadores
 
 GEIH_ocupados<-data.frame()
-GEIH_ocupados<-subset(GEIH, age>=18 & ocu==1)
+GEIH_ocupados<-subset(GEIH, age>=18 & ocu==1 & ingtot>0)
 
 #Creamos de variables de interes
 
@@ -71,14 +71,13 @@ GEIH_ocupados <- GEIH_ocupados %>%
          age_sex2 = age2*sex,
          log_Ing = log(ingtot),
          formal_sex = formal*sex,
-         realb_sex = relab*sex,
-         educ2=educ^2
-         )
+         realb_sex = relab*sex
+                  )
 
 
 ###codificación de nombre de variables
 
-GEIH_clean <- GEIH_clean %>%
+GEIH_ocupados <- GEIH_ocupados %>%
   rename(tiempo_tra = p6426, tipo_ocu  = relab, urbano = clase)  
 
 
@@ -86,9 +85,9 @@ GEIH_clean <- GEIH_clean %>%
 
 GEIH_clean <- subset(GEIH_ocupados, select = c ( Var.1, dominio, sex, ingtot, age, age2, 
                                                  age_sex, age_sex2, formal,
-                                                 relab,maxEducLevel, depto,
-                                                 clase, p6426, log_Ing, formal_sex,
-                                                 realb_sex, educ, educ2, urbano, tipo_ocupa, tiempo_tra))
+                                                 maxEducLevel, depto,
+                                                 tiempo_tra, log_Ing, formal_sex,
+                                                 realb_sex, urbano, tipo_ocu))
 ##### Variable Female 
 
 GEIH_clean<-GEIH_clean%>%
@@ -267,14 +266,60 @@ summary(modelo1)
 
 ######################################################
 
+#Definimos el escritorio para sacar tablas y gráficas#
 
-###############Corremos el modelo
-modelo2<-lm(log_Ing~female, data=GEIH_clean)
+setwd("C:/Users/pcere/Dropbox/Machine Learning/ML-TALLER1/Views")
+
+#4.1 modelo simple log_Ing ~ female
+
+modelo2 <- lm(log_Ing~female, data=GEIH_clean)
 summary(modelo2)
 tabla_reg <-huxreg(modelo2)
-#huxreg(modelo2)
+quick_docx(tabla_reg, file = "tabla_regs.docx")
 
-#############Bootstrap
+#4.3 Estimate and plot the predicted age-earnings profile by gender. 
+
+#La base únicamente contiene información de Bogotá
+
+#Creamos sets de data solo con hombres y mujeres
+GEIH_mas<-subset(GEIH_clean, female==0)
+GEIH_fem<-subset(GEIH_clean, female==1)
+
+#regresión edad para hombres
+modelo2_mas <- lm(log_Ing~age, data=GEIH_mas)
+predict(modelo2_mas)
+summary(modelo2_mas)
+SE2_mas <- function(GEIH_mas, index){
+  reg_mas <- lm(log_Ing~age, data=GEIH_mas[index, ])
+  coef(reg_mas)
+}
+boot(data=GEIH_mas, SE2_mas, R=1000)
+
+#regresión edad para mujeres
+modelo2_fem <- lm(log_Ing~age, data=GEIH_fem)
+summary(modelo2_fem)
+
+SE2_fem <- function(GEIH_fem, index){
+  reg_fem <- lm(log_Ing~age, data=GEIH_fem[index, ])
+  coef(reg_fem)
+}
+boot(data=GEIH_fem, SE2_fem, R=1000)
+
+#Creamos una tabla para comparar los dos modelos
+tabla_mas_fem <-huxreg("Hombres" = modelo2_mas,
+                       "Mujeres" = modelo2_fem)
+quick_docx(tabla_mas_fem, file = "tabla_mas_fem.docx")
+
+#Sacamos la gráfica fachera de comparación
+ggplot()+geom_point(data=GEIH_mas , aes(x=age, y=predict(modelo2_mas), color = "Hombres" ))+
+  geom_point(data=GEIH_fem , aes(x=age, y=predict(modelo2_fem), color = "Mujeres"))+
+  theme(panel.background = element_rect(fill = "white"))+
+  labs(y= "Log(Ingreso Total)", x = "Edad")+
+  theme(legend.title = element_blank())
+
+
+
+#4############Bootstrap
 
 SE2 <- function(GEIH_clean, index){
   reg <- lm(log_Ing~female, data=GEIH_clean[index, ])
@@ -282,53 +327,36 @@ SE2 <- function(GEIH_clean, index){
 }
 boot(data=GEIH_clean, SE2, R=1000)
 
-
-###################
-#La base unicamente contiene informacion de Bogota
-##############
-#Creamos sets de data solo con hombres y mujeres
-GEIH_mas<-subset(GEIH_clean, female==0)
-GEIH_fem<-subset(GEIH_clean, female==1)
-
-#regresiÃ³n edad para hombres
-modelo2_mas <- lm(log_Ing~age, data=GEIH_mas)
-predict(modelo2_mas)
-summary(modelo2_mas)
-SE2_mas <- function(GEIH_mas, index){
-  coef(lm(log_Ing~age, data=GEIH_mas), data=GEIH_mas, subset = index)
-}
-boot(data=GEIH_mas, SE2_mas, R=1000)
-
-#regresiÃ³n edad para mujeres
-modelo2_fem <- lm(log_Ing~age, data=GEIH_fem)
-summary(modelo2_fem)
-
-SE2_fem <- function(GEIH_fem, index){
-  coef(lm(log_Ing~age, data=GEIH_fem), data=GEIH_fem, subset = index)
-}
-boot(data=GEIH_fem, SE2_fem, R=1000)
-
-
-grafica_sexo<-data.frame()
-grafica_sexo<- GEIH_mas%>%{
-  mutate(Fem=  predict(lm(log_Ing~age, data=GEIH_fem)))
-}
-
-###################
-
-#### se crea el valor predicho del ingreso total es decir el "y gorro"
-library("ggplot2")
-
-ggplot()+geom_point(data=GEIH_mas , aes(x=age, y=predict(modelo2_mas)), colours="#69b3a2")+
-  geom_point(data=GEIH_fem , aes(x=age, y=predict(modelo2_fem)))
-
-##################33
-
-##########4.5.4
-
-modelo3<-lm(log_Ing~female+age+relab+formal+educ+p6426, data=GEIH_clean)
+##########4.5
+#4.5 (a) Regresión con múltiples controles
+modelo3<-lm(log_Ing~female+age+tipo_ocu+formal+educ+tiempo_tra, data=GEIH_clean)
 summary(modelo3)
-huxreg(modelo3)
+#sacamos la tabla
+tabla_reg <-huxreg(modelo3)
+quick_docx(tabla_reg, file = "tabla_reg41.docx")
+
+
+
+#4.5 (b) Demostración del teorema FWL
+
+#FWL con residuales
+#Guardamos los resiguales de log_Inc en los controles
+resi_dente <- residuals(lm(log_Ing ~ age+tipo_ocu+formal+educ+tiempo_tra, GEIH_clean))
+
+#Guardamos los resiguales de female en los controles
+female_resi <- residuals(lm(female ~ age+tipo_ocu+formal+educ+tiempo_tra, GEIH_clean))
+
+#Corremos la regresión con transofrmaciones
+FWL_funcional <- lm(resi_dente ~ female_resi, GEIH_clean)
+summary(FWL_funcional)
+
+#Sacamos la tabla
+tabla_FWL <-huxreg("Modelo completo" = modelo3,
+                   "FWL" = FWL_funcional)
+quick_docx(tabla_FWL, file = "tabla_regFWL.docx")
+
+
+
 
 
 
@@ -497,7 +525,18 @@ print(model13)
 print(model12)
 print(model11)
 
+##5.c LOOCV
+#La idea es crear N samples de data eliminando la observación j del loop aprovechando la variable Var.1
+#Luego, se guardan los predichos en una matriz para calcular el average mean square error
+LOOCVmatrix <- c()
+for (j in 1:16277){
+  
+  GEIH_LOOCV_j <- subset(GEIH_clean, Var=! j)
 
+LOOCVmatrix[j] <-predict(modelo8, GEIH_LOOCV_j)
+
+  
+  }
 
 
 
